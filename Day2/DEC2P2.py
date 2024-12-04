fe43872da3790a81ebb4c001f65d62d38f174439
Lorddread <lorddread7@yahoo.com>
2024-12-03 20:20:00 -0500

    Dec 2 Part 2 upload still not done

--- Day2/DEC2P2.py
+++ Day2/DEC2P2.py
@@ -8,41 +8,35 @@ def reportSearch (increase, line):
         print("increasing")
     else:
         print("decreasing")
-    #print()
     iterate = iter(line)
     for n in range(1, len(line)):
-        #print("last value = "+ str(lastValue))
-        #print("working on n = " + str(line[n]))
         if n == len(line) - 1 and failsafe:
-            #print("Safe with failsafe")
             return True
         if increase:
             if lastValue - int(line[n]) < -3 or lastValue - int(line[n]) > -1:
                 if n <= len(line) - 2:
-                    if lastValue - int(line[n + 1]) >= -3 and lastValue - int(line[n + 1]) <= -1 and failsafe:
-                        #print("used failsafe1")
+                    if -3 <= lastValue - int(line[n + 1]) <= -1 and failsafe:
                         failsafe = False
                         continue
-                    elif int(line[n]) - int(line[n + 1]) >= -3 and int(line[n]) - int(line[n + 1]) <= -1 and failsafe:
+                    if -3 <= int(line[n]) - int(line[n + 1]) <= -1 and failsafe:
                         failsafe = False
-                        #print("used failsafe2")
                         lastValue = int(line[n])
                         next(iterate)
-                        next(iterate)
+                        continue
                 return False
         elif not increase:
             if lastValue - int(line[n]) > 3 or lastValue - int(line[n]) < 1:
                 if n <= len(line) - 2:
-                    if lastValue - int(line[n + 1]) <= 3 and lastValue - int(line[n + 1]) >= 1 and failsafe:
+                    if 3 >= lastValue - int(line[n + 1]) >= 1 and failsafe:
                         #print("used failsafe1")
                         failsafe = False
                         continue
-                    elif int(line[n]) - int(line[n + 1]) <= 3 and int(line[n]) - int(line[n + 1]) >= 1 and failsafe:
+                    if 3 >= int(line[n]) - int(line[n + 1]) >= 1 and failsafe:
                         failsafe = False
                         #print("used failsafe2")
                         lastValue = int(line[n])
                         next(iterate)
-                        next(iterate)
+                        continue
                 return False
         lastValue = int(line[n])
         #print()
@@ -54,9 +48,17 @@ for each in lines:
     line = each.split()
     print("---------------------")
     print(line)
-    if int(line[0]) < int(line[-1]) or int(line[1]) < int(line[2]):
+    if int(line[0]) == int(line[-1]):
+        if int(line[1]) < int(line[-2]):
+            increase = True
+        elif int(line[1]) > int(line[-2]):
+            increase = False
+        else:
+            print("not safe at all")
+            continue
+    elif (int(line[0]) < int(line[-1]) or int(line[0]) < int(line[-2])):
         increase = True
-    elif int(line[0]) > int(line[-1]) or int(line[1]) > int(line[2]):
+    elif (int(line[0]) > int(line[-1]) or int(line[0]) > int(line[-2])):
         increase = False
     else:
         print("not safe at all")
